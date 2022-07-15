0c148575e7830ef3e085ba4331822ee3d57c10bd
VatBane <70464844+VatBane@users.noreply.github.com>
2022-07-15 13:08:41 +0300

    get single User

--- controllers/users.js
+++ controllers/users.js
@@ -3,20 +3,28 @@ const crypto = require('crypto-js')
 const passPhrase = 'silvio';
 
 const getUser = async (req, res) => {
-  const bytes = crypto.AES.decrypt(encodedPass, passPhrase);
-  const originalText = bytes.toString(crypto.enc.Utf8);
-  console.log(originalText);
+  try {
+    const { login } = req.params
+    const user = await User.findOne({ login });
+    res.status(201).json({ user });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+  // const bytes = crypto.AES.decrypt(encodedPass, passPhrase);
+  // const originalText = bytes.toString(crypto.enc.Utf8);
+  // console.log(originalText);
 };
 
 const getAllUsers = async (req, res) => {
   try {
     const users = await User.find({});
+    res.status(201).json({ users });
   } catch (error) {
-    res.status(500).json({ error });
+    res.status(500).json({ msg: error });
   }
-  const bytes = crypto.AES.decrypt(encodedPass, passPhrase);
-  const originalText = bytes.toString(crypto.enc.Utf8);
-  console.log(originalText);
+  // const bytes = crypto.AES.decrypt(encodedPass, passPhrase);
+  // const originalText = bytes.toString(crypto.enc.Utf8);
+  // console.log(originalText);
 }
 
 const createUser = async (req, res) => {
@@ -27,7 +35,7 @@ const createUser = async (req, res) => {
     res.status(201).json({ user });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error });
+    res.status(500).json({ msg: error });
   }
 }
 
--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {
+  getAllUsers,
+  getUser,
   createUser
 } = require('../controllers/users')
 
-router.route('/').get().post(createUser);
-
+router.route('/').get(getAllUsers).post(createUser);
+router.route('/:login').get(getUser);
 module.exports = router;
